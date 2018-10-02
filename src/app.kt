21e8164f1910aa96730ef8f4e1a02206f07835fc
idhammi <idham.m.irfani@gmail.com>
2018-10-02 21:24:43 +0700

    [idham] create calc func

--- src/app.kt
+++ src/app.kt
@@ -1,4 +1,16 @@
 
 fun main(args: Array<String>){
-    print("Hello World")
+    println("Calculator")
+    println(penjumlahan(1, 2))
+    println(pengurangan(1, 2))
+    println(pembagian(1f, 2f))
+    println(perkalian(1, 2))
 }
+
+fun penjumlahan(a: Int, b: Int): Int = a + b
+
+fun pengurangan(a: Int, b: Int): Int = a - b
+
+fun pembagian(a: Float, b: Float): Float = a / b
+
+fun perkalian(a: Int, b: Int): Int = a * b
